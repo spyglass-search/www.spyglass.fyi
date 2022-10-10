fd9fac8eaaa6aee06feb4c463ffb53384f896b8e
Andrew Huynh <a5.t.huynh@gmail.com>
2022-10-10 10:34:12 -0700

    fixing mobile nav menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,19 +12,9 @@ window.addEventListener("load", function () {
       document.querySelector("#mobileNav").classList.add("hidden");
     });
 
-  document.querySelectorAll("[toggleElement]").forEach((toggle) => {
-    toggle.addEventListener("click", function (event) {
-      console.log(toggle);
-      const answerElement = toggle.querySelector("[answer]");
-      const caretElement = toggle.querySelector("img");
-      console.log(answerElement);
-      if (answerElement.classList.contains("hidden")) {
-        answerElement.classList.remove("hidden");
-        caretElement.classList.add("rotate-90");
-      } else {
-        answerElement.classList.add("hidden");
-        caretElement.classList.remove("rotate-90");
-      }
-    });
-  });
+  document
+    .querySelectorAll("#mobileNav .nav-link")
+    .forEach((node) => node.addEventListener("click", function (event) {
+      document.querySelector("#mobileNav").classList.add("hidden");
+    }));
 });
